e79e51df404fd4ee10760ff6497aea81a24793b0
duyanhDev <loveeasy20@gmail.com>
2024-06-11 15:47:33 +0700

    check 6-11

--- src/App.js
+++ src/App.js
@@ -2,7 +2,51 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Silderleft from "./components/SliderLeft/Silderleft";
 import { Outlet } from "react-router-dom";
+import { FaHome, FaBars, FaFire } from "react-icons/fa";
+import { useEffect, useState, useRef } from "react";
+import Bowser from "bowser";
 function App() {
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    const osName = browser.getOSName();
+    const handleResize = () => {
+      setScreenSize({
+        window: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleSliderBar = () => {
+    setShow((prevShow) => !prevShow);
+  };
+
+  const HomeBtn = useRef(null);
+
+  const HandleCheckBtn = (e) => {
+    if (HomeBtn.current && !HomeBtn.current.contains(e.target)) {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", HandleCheckBtn);
+
+    return () => {
+      document.removeEventListener("mousedown", HandleCheckBtn);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +54,31 @@ function App() {
       </header>
 
       <div className="content-container">
-        <div className="silde-left">
-          <Silderleft />
+        <div className="silde-left" ref={HomeBtn}>
+          {screenSize.width >= 390 && screenSize.width <= 840 ? (
+            show && <Silderleft />
+          ) : (
+            <Silderleft />
+          )}
         </div>
         <div className="mobile:w-full pc:w-3/4 content  ">
-          <div className="mobile:w-full movie-container bg-[#1d3042]  ">
+          <div className="movie-container bg-[#1d3042]  ">
             <Outlet />
           </div>
         </div>
       </div>
+
+      {screenSize.width >= 390 && screenSize.width <= 840 && (
+        <div className="slider-bottom">
+          <FaHome className="text-xl text-[#fff]" />
+          <FaBars
+            className="text-xl text-[#fff]"
+            onClick={() => handleSliderBar()}
+          />
+          <FaFire className="text-xl text-[#fff]" />
+          <FaHome className="text-xl text-[#fff]" />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,13 @@ import image11 from "../../assets/img/11.jpg";
 import { useEffect, useState } from "react";
 
 const Header = (props) => {
-  const Navigate = useNavigate();
-  let location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
   const [name, setName] = useState("");
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
   useEffect(() => {
     if (location.state && location.state.user) {
@@ -18,155 +22,175 @@ const Header = (props) => {
         setName(item.username);
       });
     }
+
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, [location.state]);
 
   const handleLogout = () => {
-    Navigate("/login");
+    navigate("/login");
     setName("");
     if (location.state) {
       location.state.user = []; // Reset the user state
     }
   };
-  console.log(name);
+
   return (
-    <div className="container-header flex justify-between items-center mx-2">
-      <div className="header-title">
-        <p className="text-[#fff] font-bold text-2xl">
-          ANIME<span className="text-[#35f36b]">AZ</span>
-        </p>
-      </div>
-
-      <Tippy
-        interactive
-        trigger="click"
-        render={(attrs) => (
-          <div className="serach_fiter" {...attrs}>
-            <div className="search_title flex items-center  text-[#f0e2e2] font-normal text-xl cursor-pointer">
-              {" "}
-              <CiSearch />
-              Kết quả tìm kiếm từ khóa
-            </div>
-            <div className=" flex justify-between mt-3 border-b-2">
-              <span className="text-[#f0e2e2] font-bold  text-xs cursor-pointer">
-                ANIME
-              </span>
-              <span className="text-[#f0e2e2] font-bold  text-xs cursor-pointer">
-                XEM THÊM
-              </span>
-            </div>
-
-            <div className="content-search">
-              <Link className="flex">
-                <img
-                  src={image11}
-                  className="w-12 h-12 object-cover"
-                  alt="loi"
-                />
-                <span className="text-[#fff] font-light ml-2">
-                  Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou Medereba
-                  Ii?
-                </span>
-              </Link>
-              <p>
-                Khi phù thủy Zagan quyết định tham gia vào một cuộc đấu giá bán
-                hàng hóa của Archdemon Marchosias hiện đã chết, anh ta hy vọng
-                sẽ tìm thấy những vật phẩm có sức mạnh chưa từng thấy. Thay vào
-                đó, những gì anh ta tìm thấy là một yêu tinh nô lệ tóc trắng
-                hiếm hoi tên là Nephilia, và anh ta ngay lập tức chi tất cả tiền
-                của mình để mua cô ấy, khiến những người xung quanh hoang mang.
-                Tuy nhiên, lý do bí mật đằng sau khoản đầu tư của Zagan là anh
-                đã thực sự yêu cô từ cái nhìn đầu tiên. Thật không may, Zagan có
-                một vấn đề: anh ta kém cỏi về mặt xã hội và không biết cách thể
-                hiện cảm xúc thật của mình, dẫn đến nhiều hiểu lầm giữa anh ta
-                và người bạn đồng hành mới. Nhưng dần dần, cả hai bắt đầu hiểu
-                nhau, và tình yêu bắt đầu nở rộ..
-              </p>
-            </div>
-            <div className="content-search">
-              <Link className="flex">
-                <img
-                  className="w-12 h-12 object-cover"
-                  src={image11}
-                  alt="loi"
-                />
-                <span className="text-[#fff] font-light ml-2">
-                  Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou Medereba
-                  Ii?
-                </span>
-              </Link>
-              <p>
-                Khi phù thủy Zagan quyết định tham gia vào một cuộc đấu giá bán
-                hàng hóa của Archdemon Marchosias hiện đã chết, anh ta hy vọng
-                sẽ tìm thấy những vật phẩm có sức mạnh chưa từng thấy. Thay vào
-                đó, những gì anh ta tìm thấy là một yêu tinh nô lệ tóc trắng
-                hiếm hoi tên là Nephilia, và anh ta ngay lập tức chi tất cả tiền
-                của mình để mua cô ấy, khiến những người xung quanh hoang mang.
-                Tuy nhiên, lý do bí mật đằng sau khoản đầu tư của Zagan là anh
-                đã thực sự yêu cô từ cái nhìn đầu tiên. Thật không may, Zagan có
-                một vấn đề: anh ta kém cỏi về mặt xã hội và không biết cách thể
-                hiện cảm xúc thật của mình, dẫn đến nhiều hiểu lầm giữa anh ta
-                và người bạn đồng hành mới. Nhưng dần dần, cả hai bắt đầu hiểu
-                nhau, và tình yêu bắt đầu nở rộ..
-              </p>
-            </div>
-            <div className="content-search">
-              <Link className="flex">
-                <img
-                  className="w-12 h-12 object-cover"
-                  src={image11}
-                  alt="anhloi"
-                />
-                <span className="text-[#fff] font-light ml-2">
-                  Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou Medereba
-                  Ii?
-                </span>
-              </Link>
-              <p>
-                Khi phù thủy Zagan quyết định tham gia vào một cuộc đấu giá bán
-                hàng hóa của Archdemon Marchosias hiện đã chết, anh ta hy vọng
-                sẽ tìm thấy những vật phẩm có sức mạnh chưa từng thấy. Thay vào
-                đó, những gì anh ta tìm thấy là một yêu tinh nô lệ tóc trắng
-                hiếm hoi tên là Nephilia, và anh ta ngay lập tức chi tất cả tiền
-                của mình để mua cô ấy, khiến những người xung quanh hoang mang.
-                Tuy nhiên, lý do bí mật đằng sau khoản đầu tư của Zagan là anh
-                đã thực sự yêu cô từ cái nhìn đầu tiên. Thật không may, Zagan có
-                một vấn đề: anh ta kém cỏi về mặt xã hội và không biết cách thể
-                hiện cảm xúc thật của mình, dẫn đến nhiều hiểu lầm giữa anh ta
-                và người bạn đồng hành mới. Nhưng dần dần, cả hai bắt đầu hiểu
-                nhau, và tình yêu bắt đầu nở rộ..
-              </p>
-            </div>
-          </div>
-        )}
-      >
-        <div className="header-search w[20%]  flex relative">
-          <input
-            type="text"
-            className="input-search w-3/5 bg-[#353340] outline-white h-8 rounded-lg "
-            placeholder="Tìm kiếm anime"
-          />
-          <span className="icon-search">
-            <CiSearch />
-          </span>
+    <>
+      <div className="container-header flex justify-between items-center mx-2">
+        <div className="sm:ml-5 header-title ">
+          <p className="text-[#fff] font-bold text-2xl">
+            ANIME<span className="text-[#35f36b]">AZ</span>
+          </p>
         </div>
-      </Tippy>
 
-      <div className="header-login">
-        <span className="icon-login ">
-          {" "}
-          <IoLogOutOutline />
-        </span>
-        {name && name.length > 0 ? (
-          <p className="text-[#333]" onClick={() => handleLogout()}>
-            {name}
-          </p>
-        ) : (
-          <p className="text-[#333]" onClick={() => Navigate("/login")}>
-            Đăng Nhập
-          </p>
-        )}
+        <Tippy
+          interactive
+          trigger="click"
+          render={(attrs) => (
+            <div className="serach_fiter" {...attrs}>
+              <div className="search_title flex items-center text-[#f0e2e2] font-normal text-xl cursor-pointer">
+                <CiSearch />
+                Kết quả tìm kiếm từ khóa
+              </div>
+              <div className="flex justify-between mt-3 border-b-2">
+                <span className="text-[#f0e2e2] font-bold text-xs cursor-pointer">
+                  ANIME
+                </span>
+                <span className="text-[#f0e2e2] font-bold text-xs cursor-pointer">
+                  XEM THÊM
+                </span>
+              </div>
+
+              <div className="content-search">
+                <Link className="flex">
+                  <img
+                    src={image11}
+                    className="w-12 h-12 object-cover"
+                    alt="loi"
+                  />
+                  <span className="text-[#fff] font-light ml-2">
+                    Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou
+                    Medereba Ii?
+                  </span>
+                </Link>
+                <p>
+                  Khi phù thủy Zagan quyết định tham gia vào một cuộc đấu giá
+                  bán hàng hóa của Archdemon Marchosias hiện đã chết, anh ta hy
+                  vọng sẽ tìm thấy những vật phẩm có sức mạnh chưa từng thấy.
+                  Thay vào đó, những gì anh ta tìm thấy là một yêu tinh nô lệ
+                  tóc trắng hiếm hoi tên là Nephilia, và anh ta ngay lập tức chi
+                  tất cả tiền của mình để mua cô ấy, khiến những người xung
+                  quanh hoang mang. Tuy nhiên, lý do bí mật đằng sau khoản đầu
+                  tư của Zagan là anh đã thực sự yêu cô từ cái nhìn đầu tiên.
+                  Thật không may, Zagan có một vấn đề: anh ta kém cỏi về mặt xã
+                  hội và không biết cách thể hiện cảm xúc thật của mình, dẫn đến
+                  nhiều hiểu lầm giữa anh ta và người bạn đồng hành mới. Nhưng
+                  dần dần, cả hai bắt đầu hiểu nhau, và tình yêu bắt đầu nở rộ..
+                </p>
+              </div>
+              <div className="content-search">
+                <Link className="flex">
+                  <img
+                    className="w-12 h-12 object-cover"
+                    src={image11}
+                    alt="loi"
+                  />
+                  <span className="text-[#fff] font-light ml-2">
+                    Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou
+                    Medereba Ii?
+                  </span>
+                </Link>
+                <p>
+                  Khi phù thủy Zagan quyết định tham gia vào một cuộc đấu giá
+                  bán hàng hóa của Archdemon Marchosias hiện đã chết, anh ta hy
+                  vọng sẽ tìm thấy những vật phẩm có sức mạnh chưa từng thấy.
+                  Thay vào đó, những gì anh ta tìm thấy là một yêu tinh nô lệ
+                  tóc trắng hiếm hoi tên là Nephilia, và anh ta ngay lập tức chi
+                  tất cả tiền của mình để mua cô ấy, khiến những người xung
+                  quanh hoang mang. Tuy nhiên, lý do bí mật đằng sau khoản đầu
+                  tư của Zagan là anh đã thực sự yêu cô từ cái nhìn đầu tiên.
+                  Thật không may, Zagan có một vấn đề: anh ta kém cỏi về mặt xã
+                  hội và không biết cách thể hiện cảm xúc thật của mình, dẫn đến
+                  nhiều hiểu lầm giữa anh ta và người bạn
+                </p>
+              </div>
+              <div className="content-search">
+                <Link className="flex">
+                  <img
+                    className="w-12 h-12 object-cover"
+                    src={image11}
+                    alt="anhloi"
+                  />
+                  <span className="text-[#fff] font-light ml-2">
+                    Maou no Ore ga Dorei Elf wo Yome ni Shitanda ga, Dou
+                    Medereba Ii?
+                  </span>
+                </Link>
+                <p>
+                  Khi phù thủy Zagan quyết định tham gia vào một cuộc đấu giá
+                  bán hàng hóa của Archdemon Marchosias hiện đã chết, anh ta hy
+                  vọng sẽ tìm thấy những vật phẩm có sức mạnh chưa từng thấy.
+                  Thay vào đó, những gì anh ta tìm thấy là một yêu tinh nô lệ
+                  tóc trắng hiếm hoi tên là Nephilia, và anh ta ngay lập tức chi
+                  tất cả tiền của mình để mua cô ấy, khiến những người xung
+                  quanh hoang mang. Tuy nhiên, lý do bí mật đằng sau khoản đầu
+                  tư của Zagan là anh đã thực sự yêu cô từ cái nhìn đầu tiên.
+                  Thật không may, Zagan có một vấn đề: anh ta kém cỏi về mặt xã
+                  hội và không biết cách thể hiện cảm xúc thật của mình, dẫn đến
+                  nhiều hiểu lầm giữa anh ta và người bạn
+                </p>
+              </div>
+            </div>
+          )}
+        >
+          <div className="header-search w-[20%] flex relative ">
+            <input
+              type="text"
+              className="input-search w-3/5 bg-[#353340] outline-white h-8 rounded-lg  "
+              placeholder="Tìm kiếm anime"
+            />
+            <span className="icon-search">
+              <CiSearch />
+            </span>
+          </div>
+        </Tippy>
+
+        <div className="header-login">
+          <span className="icon-login">
+            <IoLogOutOutline />
+          </span>
+          {name && name.length > 0 ? (
+            <p className="text-[#333]" onClick={handleLogout}>
+              {name}
+            </p>
+          ) : (
+            <p
+              className="text-[#333]"
+              style={{
+                display:
+                  screenSize.width <= 390 && screenSize.height === 844
+                    ? "none"
+                    : "block",
+              }}
+              onClick={() => navigate("/login")}
+            >
+              Đăng Nhập
+            </p>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
